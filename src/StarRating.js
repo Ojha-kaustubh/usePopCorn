49a864e5d178cc726c8e8e0cc683552ea3fdcdf4
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-03-18 18:36:40 +0530

    Hover Handle on Star

--- src/StarRating.js
+++ src/StarRating.js
@@ -16,6 +16,7 @@ const textStyle = {
 };
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(1);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleStar(rating) {
     setRating(rating);
@@ -25,10 +26,16 @@ export default function StarRating({ maxRating = 5 }) {
     <div style={containerStyle}>
       <div style={starContainer}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={i} onClick={() => handleStar(i + 1)} full={i + 1 <= rating} />
+          <Star
+            key={i}
+            onClick={() => handleStar(i + 1)}
+            full={tempRating ? tempRating >= i + 1 : i + 1 <= rating}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+          />
         ))}
       </div>
-      <p style={textStyle}>{rating || " "} </p>
+      <p style={textStyle}>{tempRating || rating || " "} </p>
     </div>
   );
 }
@@ -41,9 +48,14 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onClick, full }) {
+function Star({ onClick, full, onHoverIn, onHoverOut }) {
   return (
-    <span style={starStyle} onClick={onClick}>
+    <span
+      style={starStyle}
+      onClick={onClick}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
