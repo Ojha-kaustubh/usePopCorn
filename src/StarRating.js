9a9c36feffae97544ea66c8b6d8c241bfee67154
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-03-19 22:25:23 +0530

    Improvind the reusablity of the stars

--- src/StarRating.js
+++ src/StarRating.js
@@ -10,16 +10,22 @@ const starContainer = {
   display: "flex",
 };
 
+
+
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   size = 48,
+  message = [],
+  defaultRating = 0,
+  onSetRating,
 }) {
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleStar(rating) {
     setRating(rating);
+    onSetRating(rating)
   }
 
   const textStyle = {
@@ -44,15 +50,20 @@ export default function StarRating({
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || " "} </p>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? message[tempRating ? tempRating -1 : rating - 1]
+          : tempRating || rating || " "}{" "}
+      </p>
     </div>
   );
 }
 
-function Star({ onClick, full, onHoverIn, onHoverOut , color , size }) {
+function Star({ onClick, full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
     fill: "yellow",
     width: `${size}px`,
+
     height: `${size}px`,
     display: "block",
     cursor: "pointer",
@@ -69,7 +80,7 @@ function Star({ onClick, full, onHoverIn, onHoverOut , color , size }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill={color}     
+          fill={color}
           stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -79,7 +90,7 @@ function Star({ onClick, full, onHoverIn, onHoverOut , color , size }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
 
-import StarRating from './StarRating';  
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+
+  const [movieRating, setMovieRating] = React.useState(0);
+
+  return <div>
+    <StarRating maxRating={10} color="blue" onSetRating={setMovieRating}/>
+    <p>This Movie Was Rated {movieRating} Stars</p>
+  </div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <Test />
+    <StarRating
+      maxRating={5}
+      message={["Terrible ", "Bad", "Okay", "Good", "Excellent"]}
+    />
+    <StarRating size={24} color="orange" className="test" defaultRating={3} />
   </React.StrictMode>
 );
 
